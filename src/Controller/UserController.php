6eafec7ea354467212fbc7a719506c9ca81ac024
Franak007 <franck.pertosa@gmail.com>
2023-11-08 23:45:46 +0100

    f

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,28 +13,28 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
-#[Route('/api/users')]
+//#[Route('/api/user')]
 class UserController extends AbstractController
 {
     public function __construct(
-        private Security $security
+        private Security $security,
     )
     {
     }
 
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
         $user = $userRepository->findAll();
         return $this->json($user, 200, [], ['groups' => 'user:read']);
     }
 
-    #[Route('/whoami', name: 'app_user_whoami', methods: ['GET'])]
+    #[Route('/api/user/whoami', name: 'user_whoami', methods: ['GET'])]
 
     public function whoAmI():JsonResponse
     {
         $user = $this->security->getUser();
 
         return $this->json($user);
+
     }
 }
